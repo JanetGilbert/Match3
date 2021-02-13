bdfac10a03dfe2b159cd9079e96dbee34cea50cc
Janet Gilbert <janet@wadjeteyegames.com>
2021-02-13 10:12:55 -0500

    Added camera focus

---
+++ Assets/Scripts/CameraFit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Fits orthographic camera to display area within a Bounds.
+public class CameraFit : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    // Based on https://answers.unity.com/questions/1231701/fitting-bounds-into-orthographic-2d-camera.html
+    // User Satchel82
+    public void Fit(Bounds bounds)
+    {
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = bounds.size.x / bounds.size.y;
+
+        if (screenRatio >= targetRatio)
+        {
+            Camera.main.orthographicSize = bounds.size.y / 2;
+        }
+        else
+        {
+            float differenceInSize = targetRatio / screenRatio;
+            Camera.main.orthographicSize = bounds.size.y / 2 * differenceInSize;
+        }
+
+        transform.position = new Vector3(bounds.center.x, bounds.center.y, -1f);
+    }
+}
--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -2,22 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AnimalType { Cat, Chick, Fox, Mouse, Pig, Rabbit };
+
+[System.Serializable]
+public struct AnimalDef
+{
+    public AnimalType type;
+    public Sprite sprite;
+}
+
+
+
 public class GameBoard : MonoBehaviour
 {
-    public Animal animalPrefab;
-    public int gridSizeX;
-    public int gridSizeY;
-    public float gridSpacing; // Space between 
 
+
+    // Set in Editor
+    public Animal animalPrefab; // Prefab of animal game piece.
+    public int gridX; // Horizontal grid size.
+    public int gridY; // Vertical grid size.
+    public float gridSpacing; // Space between animals in grid.
+
+    public AnimalDef [] animalDefs;
+
+    private Animal [,] animalGrid;
 
     void Start()
     {
-        
+        InitGrid();
+
+       
     }
 
     
     void Update()
     {
         
+    }
+
+    private void InitGrid()
+    {
+        float animalPosX = transform.position.x;
+        float animalPosY = transform.position.y;
+
+        // Create random grid.
+        animalGrid = new Animal[gridX, gridY];
+
+        for (int x = 0; x < gridX; x++)
+        {
+            for (int y = 0; y < gridY; y++)
+            {
+                animalGrid[x, y] = Instantiate(animalPrefab, new Vector3(animalPosX, animalPosY, 0), Quaternion.identity);
+                animalGrid[x, y].name = "Animal_" + x + "_" + y;
+                animalGrid[x, y].transform.parent = this.transform; // Set the Board as the parent of the grid items.
+
+                animalPosY += gridSpacing;
+            }
+
+            animalPosY = transform.position.y;
+            animalPosX += gridSpacing;
+        }
+
+        // Fit to screen.
+        CameraFit cameraFit = Camera.main.GetComponent<CameraFit>();
+        float halfGridWidth = gridX * gridSpacing * 0.5f;
+        float halfGridHeight = gridY * gridSpacing * 0.5f;
+        float halfSpacing = gridSpacing / 2.0f;
+
+        Bounds bounds = new Bounds();
+        bounds.center = transform.position + new Vector3(halfGridWidth - halfSpacing, halfGridHeight - halfSpacing);
+        bounds.extents = new Vector3(halfGridWidth, halfGridHeight, 0.0f);
+
+        cameraFit.Fit(bounds);
     }
 }
